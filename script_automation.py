7b84a5f0f1aa7be5a9b5274f2f32732791e4edea
lucasSena <smartsena@gamail.com>
2023-10-25 18:35:13 -0300

    Create function to create catalog

--- script_automation.py
+++ script_automation.py
@@ -45,32 +45,31 @@ class ColorCompositonLC08:
         return unprocessed_scenes
 
 
-dd = './Download_scenes_v/'
-# dd = './Download_scenes/pasta_xpto2/'
-for scene in unprocessed_scenes:
-  satellite, colection, orbita, ponto = scene[:4], scene[35:37],scene[10:13],scene[13:16]
-  path_scene = os.path.join(satellite, colection, orbita, ponto, scene)
-  band4, band5, band6, mtl_scene, scene_composition  = dd + scene + '_B4.TIF', dd + scene + '_B5.TIF', dd + scene + '_B6.TIF', dd + scene + '_MTL.txt', os.path.join(path_scene, scene + '_R6G5B4.TIF')
-  files_scene = [band4, band5, band6, mtl_scene,]
-  print(band5+"Foi---------<<<<<<<<<<")
-#   import pdb; pdb.set_trace()
-  if os.path.exists(band4) and os.path.exists(band5) and os.path.exists(band6) and os.path.exists(mtl_scene):
-    #se o diretório existe e se o scene_composition ainda não foi gerado
-    os.makedirs(path_scene, exist_ok=True)
-    if os.path.isdir(path_scene) and not os.path.exists(os.path.join(path_scene,os.path.split(scene_composition)[1])):
-        cmd="gdal_merge.py -ot Float32 -of GTiff -separate -o " + scene_composition
-        demList = [band6, band5, band4]
-        subprocess.call(cmd.split()+demList)
-        print("----------------------->>>>>>>>>>>>>>" + demList[1])
+    def create_catalog (self):
+        unprocessed_scenes= self.list_unprocessed_scenes()
         # import pdb; pdb.set_trace()
-        for file_scene in files_scene:
-          #cria a pasta
-          if not os.path.exists(os.path.join(path_scene,os.path.split(file_scene)[1])):
-            #move o arquivo
-            # shutil.move('./Download_scenes/pasta_xpto2/', './LC08x/foi/foiDeNovo')
-            shutil.copy(os.path.join(dd,os.path.split(file_scene)[1]), path_scene)
-
+        for scene in unprocessed_scenes:
+          satellite, colection, orbita, ponto, ano = scene[:4], scene[35:37],scene[10:13],scene[13:16],scene[17:21]
+          path_scene = os.path.join(satellite, colection, orbita, ponto, scene)
+          band4, band5, band6, mtl_scene, scene_composition  =os.path.join(self.path_download, scene + '_B4.TIF'), os.path.join(self.path_download, scene + '_B5.TIF'), os.path.join(self.path_download, scene + '_B6.TIF'), os.path.join(self.path_download, scene + '_MTL.txt'), os.path.join(path_scene, scene + '_R6G5B4.TIF')
+          files_scene = [band4, band5, band6, mtl_scene,]
+          print(band5+"--6--Foi-----<<<<<<<<<<")
+          print(os.path.exists(band5))
+          if os.path.exists(band4) and os.path.exists(band5) and os.path.exists(band6) and os.path.exists(mtl_scene):
+            #cria a pasta
+            os.makedirs(path_scene, exist_ok=True)
+            #se o diretório existe e se o scene_composition ainda não foi gerado
+            if os.path.isdir(path_scene) and not os.path.exists(os.path.join(path_scene,os.path.split(scene_composition)[1])):
+                cmd="gdal_merge.py -ot Float32 -of GTiff -separate -o " + scene_composition
+                demList = [band6, band5, band4]
+                subprocess.call(cmd.split()+demList)
+                print("----------------------->>>>>>>>>>>>>>" + demList[1])
+                for file_scene in files_scene:
+                  if not os.path.exists(os.path.join(path_scene,os.path.split(file_scene)[1])):
+                    #move o arquivo
+                    # shutil.move('./Download_scenes/pasta_xpto2/', './LC08x/foi/foiDeNovo')
+                    shutil.copy(os.path.join(self.path_download,os.path.split(file_scene)[1]), path_scene)
 
 
 instance = ColorCompositonLC08()
-instance.list_unprocessed_scenes()
+instance.create_catalog()
